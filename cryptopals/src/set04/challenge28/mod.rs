dd6df37e4d44ea88b6bf2b733000f7a0e4ffbdf2
Andrew Bennett <andrew.bennett@toyotaconnected.com>
2019-11-16 10:57:54 -0600

    Complete Set 3, Challenge 28: https://cryptopals.com/sets/3/challenges/28

---
+++ cryptopals/src/set04/challenge28/mod.rs
@@ -0,0 +1,91 @@
+#[derive(Clone, Debug, PartialEq)]
+pub struct Sha1Mac {
+    key: Vec<u8>,
+}
+
+impl Default for Sha1Mac {
+    fn default() -> Self {
+        Self::random()
+    }
+}
+
+fn constant_time_compare<A: ?Sized + AsRef<[u8]>, B: ?Sized + AsRef<[u8]>>(a: &A, b: &B) -> bool {
+    let a = a.as_ref();
+    let b = b.as_ref();
+    if a.len() != b.len() {
+        return false;
+    }
+    a.iter().zip(b.iter()).fold(0, |acc, (a, b)| acc | (a ^ b)) == 0
+}
+
+impl Sha1Mac {
+    pub fn random() -> Self {
+        use rand::prelude::*;
+        let mut csprng = thread_rng();
+        let size: usize = csprng.gen_range(1, 128);
+        let mut key: Vec<u8> = vec![0_u8; size];
+        csprng.fill_bytes(&mut key);
+        Self::new(key)
+    }
+
+    pub fn new(key: Vec<u8>) -> Self {
+        Self { key }
+    }
+
+    pub fn authenticate<M: ?Sized + AsRef<[u8]>>(&self, message: &M) -> Vec<u8> {
+        use hash::fixed_hash::*;
+        let mut hsh = hash::sha1::Sha1Context::init();
+        hsh.update(&self.key).unwrap();
+        hsh.update(message).unwrap();
+        hsh.output().unwrap().to_vec()
+    }
+
+    pub fn validate<M: ?Sized + AsRef<[u8]>, C: ?Sized + AsRef<[u8]>>(
+        &self,
+        message: &M,
+        code: &C,
+    ) -> bool {
+        let challenge = self.authenticate(message);
+        constant_time_compare(&challenge, code)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use hash::fixed_hash::*;
+    use quickcheck::TestResult;
+
+    #[test]
+    fn implement_a_sha1_keyed_mac() {
+        let ctx = Sha1Mac::default();
+        let message = "my protected message";
+        let mac = ctx.authenticate(&message);
+        let invalid_message = "my protected messagf";
+        assert_eq!(false, ctx.validate(&invalid_message, &mac));
+        assert_eq!(true, ctx.validate(&message, &mac));
+    }
+
+    #[quickcheck]
+    fn sha1_keyed_mac_properties(key: Vec<u8>, message: Vec<u8>) -> TestResult {
+        // Verify that you cannot tamper with the message without breaking
+        // the MAC you've produced, and that you can't produce a new MAC
+        // without knowing the secret key.
+        if key.is_empty() {
+            TestResult::discard()
+        } else {
+            TestResult::from_bool(test_sha1_keyed_mac_properties(key, message))
+        }
+    }
+
+    fn test_sha1_keyed_mac_properties(key: Vec<u8>, message: Vec<u8>) -> bool {
+        let ctx = Sha1Mac::new(key);
+        let empty_mac = hash::sha1::hash(&[]).to_vec();
+        let mac = ctx.authenticate(&message);
+        if !ctx.validate(&[], &empty_mac) {
+            ctx.validate(&message, &mac)
+        } else {
+            false
+        }
+    }
+}
